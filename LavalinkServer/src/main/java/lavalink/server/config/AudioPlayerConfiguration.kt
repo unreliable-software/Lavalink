1ac7aa0d732445ac48aacf6460dc9f0d4fa2fc62
Freya Arbjerg <freya@arbjerg.dev>
2021-12-30 16:27:34 +0100

    Fix http manager being registered before plugin managers

--- LavalinkServer/src/main/java/lavalink/server/config/AudioPlayerConfiguration.kt
+++ LavalinkServer/src/main/java/lavalink/server/config/AudioPlayerConfiguration.kt
@@ -95,7 +95,7 @@ class AudioPlayerConfiguration {
         if (sources.isTwitch) audioPlayerManager.registerSourceManager(TwitchStreamAudioSourceManager())
         if (sources.isVimeo) audioPlayerManager.registerSourceManager(VimeoAudioSourceManager())
         if (sources.isMixer) audioPlayerManager.registerSourceManager(BeamAudioSourceManager())
-        if (sources.isHttp) audioPlayerManager.registerSourceManager(HttpAudioSourceManager())
+        if (sources.isLocal) audioPlayerManager.registerSourceManager(LocalAudioSourceManager())
 
         audioSourceManagers.forEach {
             audioPlayerManager.registerSourceManager(it)
@@ -111,7 +111,7 @@ class AudioPlayerConfiguration {
         }
 
         // This must be loaded last
-        if (sources.isLocal) audioPlayerManager.registerSourceManager(LocalAudioSourceManager())
+        if (sources.isHttp) audioPlayerManager.registerSourceManager(HttpAudioSourceManager())
 
         return am
     }
